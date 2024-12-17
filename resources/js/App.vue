eb7f2bb73c62d0ce1fbd6c74bd0df87e55f3beda
Pedro Vicente <pedrozwpiper@gmail.com>
2024-12-17 12:44:32 -0300

    feat(system): ambiente com vue e laravel dockernizado

---
+++ resources/js/App.vue
@@ -0,0 +1,18 @@
+<template>
+    <div id="app">
+      
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    name: "App",
+  };
+  </script>
+  
+  <style>
+  h1 {
+    color: #42b983;
+  }
+  </style>
+  
---
+++ resources/js/components/ExampleComponent.vue
@@ -0,0 +1,17 @@
+<template>
+    <div>
+        <h1>Hello from Vue.js in Laravel 10!</h1>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'ExampleComponent',
+};
+</script>
+
+<style scoped>
+h1 {
+    color: #42b983;
+}
+</style>
